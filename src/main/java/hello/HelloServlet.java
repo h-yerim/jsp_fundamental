53e1638f292389c346de810fdad63045756f095e
h-yerim <hrim1103@gmail.com>
2021-06-08 15:15:29 +0900

    2021-06-08-v1

---
+++ src/main/java/hello/HelloServlet.java
@@ -0,0 +1,30 @@
+package hello;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//http://localhost:8080/hello.kpc
+@WebServlet("/hello.kpc")
+
+public class HelloServlet extends HttpServlet {
+	@Override
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html>                        ");
+		out.println("<head>                        ");
+		out.println("<title>hello servlet</title>  ");
+		out.println("</head>                       ");
+		out.println("<body>                        ");
+		out.println("<h1>Hello Servelt</h1>        ");
+		out.println("</body>                       ");
+		out.println("</html>");
+	}
+}
